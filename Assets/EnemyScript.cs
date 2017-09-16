8ea51650cd4e1b7bb8819a9b2f62e4e1ebede68d
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-09-17 00:00:01 +0900

    init commit

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public int enemyHP1 = 100;
+//	public int enemyHP2 = 100;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("敵のHPは"+enemyHP1);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (enemyHP1 == 0) {
+			Debug.Log ("1は倒れた");
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision){
+		enemyHP1--;
+		Debug.Log ("敵1のHPは" + enemyHP1);
+	}
+}
+
+
+//collider
+//衝突した相手のコライダーが保管される。値は、Colliderクラスのインスタンス
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	int count = 0;
+	int playerHP = 10; 
+
+	void Start () {
+		Debug.Log (playerHP);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			if(gameObject.GetComponent<Rigidbody> ().velocity== new Vector3(0,0,0)){
+				gameObject.GetComponent<Rigidbody> ().velocity = transform.forward * 50f;
+				count++;
+				Debug.Log (count);
+			}
+		}
+		if (gameObject.GetComponent<Rigidbody> ().velocity == new Vector3 (0, 0, 0)) {
+			transform.Rotate (new Vector3(0, 5f, 0));
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		playerHP--;
+		Debug.Log ("プレイヤーのHPは" + playerHP);
+		if(playerHP == 0){
+			Debug.Log ("GameOver");
+		}
+	}
+}
